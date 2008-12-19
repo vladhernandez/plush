a31ab5470b64ea604f6d3f386bfdcbff2e6a378f
aglenn <aaron@nonfiction.ca>
2008-12-18 23:56:49 -0700

    Adding Observer to send emails for forms.

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -10,40 +10,67 @@ class Mailer < ActionMailer::Base
     body       :occasion_order_form => occasion_order_form
   end
 
-  def other_sample_order(sent_at = Time.now)
+  def other_sample_order(other_sample_order, sent_at = Time.now)
     subject    'Mailer#other_sample_order'
     recipients ''
     from       ''
     sent_on    sent_at
     
-    body       :greeting => 'Hi,'
+    body       :other_sample_order => other_sample_order
   end
 
-  def program_order(sent_at = Time.now)
+  def program_order(program_order, sent_at = Time.now)
     subject    'Mailer#program_order'
     recipients ''
     from       ''
     sent_on    sent_at
     
-    body       :greeting => 'Hi,'
+    body       :program_order => program_order
   end
 
-  def thank_you_order(sent_at = Time.now)
+  def thank_you_order(thank_you_order, sent_at = Time.now)
     subject    'Mailer#thank_you_order'
     recipients ''
     from       ''
     sent_on    sent_at
     
-    body       :greeting => 'Hi,'
+    body       :thank_you_order => thank_you_order
   end
 
-  def wedding_sample_order(sent_at = Time.now)
+  def wedding_sample_order(wedding_sample_order, sent_at = Time.now)
     subject    'Mailer#wedding_sample_order'
     recipients ''
     from       ''
     sent_on    sent_at
     
-    body       :greeting => 'Hi,'
+    body       :wedding_sample_order => wedding_sample_order
+  end
+
+  def invite_order(invite_order, sent_at = Time.now)
+    subject    'Mailer#invite_order'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+    
+    body       :invite_order => invite_order
+  end
+
+  def save_date(save_date, sent_at = Time.now)
+    subject    'Mailer#save_date'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+    
+    body       :save_date => save_date
+  end
+
+  def favour_tab_label_order(favour_tab_label_order, sent_at = Time.now)
+    subject    'Mailer#favour_tab_label_order'
+    recipients ''
+    from       ''
+    sent_on    sent_at
+    
+    body       :favour_tab_label_order => favour_tab_label_order
   end
 
 end
