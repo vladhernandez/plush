38d1b820996ff3b0310fc036a0b5ff7a8bd388bb
James Martens <jaemo@meerkat.localdomain>
2008-12-18 20:30:25 -0700

    updated migration: adding new fields to category

--- db/migrate/20081217053714_add_title_sub_and_descrip_to_cats.rb
+++ db/migrate/20081217053714_add_title_sub_and_descrip_to_cats.rb
@@ -1,12 +1,10 @@
 class AddTitleSubAndDescripToCats < ActiveRecord::Migration
   def self.up
-    add_column :categories, :title, :string
     add_column :categories, :subtitle, :string
     add_column :categories, :description, :text
   end
 
   def self.down
-    remove_column :categories, :title
     remove_column :categories, :subtitle
     remove_column :categories, :description
   end
