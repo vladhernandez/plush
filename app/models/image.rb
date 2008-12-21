140cf667246f0f9738ed2ac13af9887dc633e7b6
aglenn <aaron@nonfiction.ca>
2008-12-20 18:37:03 -0700

    Image Named Scopes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ActiveRecord::Base
   has_attached_file :file
   # has_attached_file :photo, :path => ":rails_root/public/:class/:attachment/:id/:style_:basename.:extension"
+  named_scope :thumb, :conditions => {:usage => 'thumb'}
+  named_scope :large, :conditions => {:usage => 'large'}
+  named_scope :xlarge, :conditions => {:usage => 'xlarge'}
 end
