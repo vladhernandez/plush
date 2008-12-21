6eb10114a9cfa5a7e636d3e29ca0f29b0d7ce212
aglenn <aaron@nonfiction.ca>
2008-12-20 21:13:01 -0700

    New Product Lightbox helper

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,4 +2,8 @@ module ImagesHelper
   def linked_lightbox_image(image)
     link_to(image_tag(image.file.url('thumb')), image.file.url('full'), :class => 'lightwindow')
   end
+  
+  def default_product_image(product)
+    link_to image_tag(product.images.large.first.file.url), product.images.xlarge.first.file.url, :class => 'lightbox' unless product.images.large.empty?
+  end
 end
